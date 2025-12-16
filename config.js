eb15e62dd8fda5fb8053c30c80333f6dc2940846
chellezheng <chellezheng@gmail.com>
2025-12-16 16:23:16 +0800

    fix: export DB_PORT in config.js to allow custom db port

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ try {
 
 module.exports = {
     DB_HOST: process.env.DB_HOST || secrets.DB_HOST || 'localhost',
+    DB_PORT: process.env.DB_PORT || secrets.DB_PORT || 3306, // Added DB_PORT support
     DB_USER: process.env.DB_USER || secrets.DB_USER || 'root',
     DB_PASSWORD: process.env.DB_PASSWORD || secrets.DB_PASSWORD,
     DB_NAME: process.env.DB_NAME || secrets.DB_NAME || 'schedule_assistant',
